6c3111e3224779f65f4ea269ed5570193cc0f3c0
Konstantin Adamov <rayadams@outlook.com>
2024-04-08 16:35:21 -0700

    Updates in use cases

--- app/src/main/java/com/rayadams/roomdbexample/use_cases/BaseUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.rayadams.roomdbexample.use_cases
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-
-open class BaseUseCase {
-    internal val coroutineScope = CoroutineScope(Dispatchers.IO)
-}
--- app/src/main/java/com/rayadams/roomdbexample/use_cases/UpdateContactUseCase.kt
+++ app/src/main/java/com/rayadams/roomdbexample/use_cases/UpdateContactUseCase.kt
@@ -2,14 +2,11 @@ package com.rayadams.roomdbexample.use_cases
 
 import com.rayadams.roomdbexample.models.ContactModel
 import com.rayadams.roomdbexample.services.DbService
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class UpdateContactUseCase @Inject constructor(private val dbService: DbService) : BaseUseCase() {
-    suspend operator fun invoke (contact:ContactModel) {
-        withContext(coroutineScope.coroutineContext) {
-            dbService.db.contactDao().update(contact)
-            dbService.notifyDataChanges()
-        }
+class UpdateContactUseCase @Inject constructor(private val dbService: DbService) {
+    operator fun invoke(contact: ContactModel) {
+        dbService.db.contactDao().update(contact)
+        dbService.notifyDataChanges()
     }
 }
