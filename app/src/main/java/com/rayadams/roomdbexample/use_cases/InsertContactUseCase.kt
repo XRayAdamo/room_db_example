10db5df60ebe8e8f3202a4b7ce5d8a1a364d9949
Konstantin Adamov <rayadams@outlook.com>
2024-04-08 18:44:30 -0700

    Revert "Updates in use cases"
    This reverts commit 6c3111e3224779f65f4ea269ed5570193cc0f3c0.

--- app/src/main/java/com/rayadams/roomdbexample/use_cases/InsertContactUseCase.kt
+++ app/src/main/java/com/rayadams/roomdbexample/use_cases/InsertContactUseCase.kt
@@ -2,11 +2,14 @@ package com.rayadams.roomdbexample.use_cases
 
 import com.rayadams.roomdbexample.models.ContactModel
 import com.rayadams.roomdbexample.services.DbService
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class InsertContactUseCase @Inject constructor(private val dbService: DbService) {
-    operator fun invoke(contact: ContactModel) {
-        dbService.db.contactDao().insert(contact)
-        dbService.notifyDataChanges()
+class InsertContactUseCase @Inject constructor(private val dbService: DbService) : BaseUseCase() {
+    suspend operator fun invoke(contact:ContactModel) {
+        withContext(coroutineScope.coroutineContext) {
+            dbService.db.contactDao().insert(contact)
+            dbService.notifyDataChanges()
+        }
     }
 }
